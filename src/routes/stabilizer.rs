949c7e9720b4d2dee502b3bd4417337fd016ded4
GHexxerBrdv <gaurang1415@gmail.com>
2026-07-08 16:09:52 +0530

    remove routes

--- src/routes/stabilizer.rs
+++ src/routes/stabilizer.rs
@@ -4,7 +4,5 @@ use crate::routes::AppState;
 
 pub fn router() -> Router<AppState> {
     Router::new()
-        .route("/api/v1/stabilizer/metrics", get(stabilizer::get_metrics))
-        .route("/api/v1/stabilizer/pools", get(stabilizer::list_pools))
-        .route("/api/v1/stabilizer/pools/{address}", get(stabilizer::get_pool_by_address))
+        .route("/api/v1/stabilizer/state", get(stabilizer::get_metrics))
 }
